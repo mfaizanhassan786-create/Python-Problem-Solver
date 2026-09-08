02fed7e6d0afeb48fa8086c7e0ce2802c5304ccb
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-08 10:04:24 +0500

    Update

---
+++ Data-types.py
@@ -0,0 +1,35 @@
+# # Program 1 — Identify Basic Data
+
+# # Statement:
+# # Create four variables:
+
+# # Your name → str
+# # Your age → int
+# # Your height → float
+# # Whether you are a student → bool
+
+# # Then print all four values.
+
+# # name = str("Faizan")
+
+# # age = int(20)
+
+# # height = float(5.11)
+
+# # student = True
+
+# # print(name)
+# # print(age)
+# # print(height)
+
+# name = str(input("Enter your name: "))
+# age = int(input("Enter your age: "))
+# height = float(input("Enter your height: "))
+# student = bool(input("You are the student: "))
+
+# # student = True
+
+# print(name)
+# print(age)
+# print(height)
+# print("Ok",student)
