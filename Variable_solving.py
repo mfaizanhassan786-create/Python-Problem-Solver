c1dd2018b807c2d3cab1a98cc84d6ea0e10545d0
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-08 09:34:06 +0500

    Add Python problem solving programs

---
+++ Variable_solving.py
@@ -0,0 +1,18 @@
+a =10 
+b = 20
+c= a+ b
+print(c)
+
+print("Hello World")
+
+
+a= 'Ali'
+b= 'Ahmad'
+
+print(a)
+print(b)
+print(a+b)
+
+print(b + a)
+
+
---
+++ input_solving.py
@@ -0,0 +1,99 @@
+
+# Program 1 — Name Greeting
+
+# Statement:
+# Write a program that asks the user for their name and then prints a welcome message containing their name.
+
+# a= input("Enter Your Name: ")
+# print("Hello", a)
+
+
+# """
+# Program 2 — Personal Information
+
+# Statement:
+#         Write a program that asks the user for their name, age, and city, then displays all the information.
+# """
+# a= input("Enter your name: ")
+# b = input("Enter your age: ")
+# c= input("Enter your city: ")
+
+# d = input("Enter Your Profession: ")
+
+# print("Name: ", a)
+# print("Age: ", b)
+# print("City: ", c)
+# print("Profession: ", d)
+
+
+# #Program 3 — Add Two Numbers
+
+# # Statement:
+# # Write a program that takes two numbers from the user and calculates their sum. 
+
+# a= input("Enter a number:")
+# b= input("Enter second number:")
+# c= a+b
+# print("The sum of the numbers:",c)
+
+# # Program 4 — Basic Calculator
+
+# # Statement:
+# # Write a program that asks the user for two numbers and calculates:
+
+# # Addition
+# # Subtraction
+# # Multiplication
+# # Division
+
+# a= float(input("Enter a number:"))
+# b= float(input("Enter second number:"))
+# c = a+b
+# d = a-b
+# e = a*b
+# f = a/b
+# print("Addition:",c)
+# print("Subtraction:",d)
+# print("Multipication:",e)
+# print("Division:",f)
+
+# # Another Way:
+
+# a = float(input("Enter the first number: "))
+# b = float(input("Enter the second number: "))
+
+# print(f"Addition: {a + b}")
+# print(f"Subtraction: {a - b}")
+# print(f"Multiplication: {a * b}")
+# print(f"Division: {a / b}")
+
+
+# # Program 5 — Student Marks
+
+# # Statement:
+# # Write a program that asks the user for marks in three subjects:
+
+# # Mathematics
+# # Physics
+# # Computer
+
+# # Calculate and display:
+
+# # Total marks
+# # Percentage
+
+# # Assume each subject is out of 100.
+
+
+# total = float(input("Enter Total numbers:"))
+# a = float(input("Enter your Obtain Math number:"))
+# b =float( input("Enter your Obtain Physics number:"))
+# c = float(input("Enter your Obtain Computer number:"))
+
+# obtain_marks = a+b+c
+# percentage = obtain_marks/total * 100
+
+# print("Your obtain Marks:", obtain_marks)
+# print("Your Percentage is", percentage)
+
+
