07480f831a7251ef904512ffe35d6c4a9962f51f
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-08 22:31:21 +0500

    Adding the new file in which the new code having in this.

---
+++ Operators.py
@@ -0,0 +1,30 @@
+# # some are the examples of the Arithmetic, comparison, assignment operators:
+
+# a = 20
+# b = 5
+
+# result = a + b
+# print(result)
+
+
+# x = 15
+# y = 8
+
+# print("x > y is", x > y)      # Comparison operator
+# print("x == y is", x == y)    # Comparison operator
+
+# a = 10   # Assignment operator
+# a += 5   # Augmented assignment operator
+# print(a)
+
+
+# x = 10
+
+# x += 5
+# x *= 2
+
+# result = x > 20
+
+# print(result)
+
+
