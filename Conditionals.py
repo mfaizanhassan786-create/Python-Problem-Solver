38232b7d9834bb024f206f1638087618e3d9bd42
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-08 22:46:15 +0500

    Updata

---
+++ Conditionals.py
@@ -0,0 +1,44 @@
+# # Problem 1 — Adult Check
+
+# # Statement:
+# # Write a program that stores a person's age and prints "You are an adult" only if the age is 18 or greater.
+
+# a = int(input("Enter your age: "))
+
+# if a >= 18:
+#     print("You are an adult")
+# else:
+#     print("You are not an adult")
+
+
+# # Problem 2 — Positive Number
+
+# # Statement:
+# # Take a number from the user and print "Positive number" only if the number is greater than 0.
+
+
+# a= int(input("Enter the number: "))
+
+# if a > 0:
+#     print("Positive number")
+# else:
+#     print("Negative number")
+
+
+# Problem 3 — Even Number
+
+# Statement:
+# Take an integer from the user.
+
+# If the number is even, print:
+
+# This is an even number.
+
+
+
+a = int(input("Enter your Number: "))
+
+if a % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
