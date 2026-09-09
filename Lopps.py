67dd60caf06c020ceb01957ed14964c2e42b4f18
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-09 12:57:30 +0500

    update

--- Lopps.py
+++ Lopps.py
@@ -14,15 +14,29 @@ for Loop — 5 Problems
 
 
 
-# Problem 2 — Multiplication Table
+# # Problem 2 — Multiplication Table
 
-# Statement:
-# Ask the user for a number and print its multiplication table from 1 to 10.
+# # Statement:
+# # Ask the user for a number and print its multiplication table from 1 to 10.
 
 
-# 1. Get user input and convert to an integer
-num = int(input("Enter a number: "))
+# # 1. Get user input and convert to an integer
+# num = int(input("Enter a number: "))
 
-# 2. Loop from 1 to 10
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")
+# # 2. Loop from 1 to 10
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
+
+
+# # Problem 3 — Sum of Numbers
+
+# # Statement:
+# # Ask the user for a number n.
+
+# # Use a for loop to calculate the sum of all numbers from 1 to n.
+
+# num = int(input("Enter a number:"))
+# sum = 0
+# for i in range(0,num+1):
+#     sum += i
+# print(f"The sum of numbers from 1 to {num} is {sum}")
