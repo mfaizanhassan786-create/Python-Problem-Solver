b63e7a4d2d7f935babe318a212075d1d4af3cfd2
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-10 10:53:47 +0500

    update

--- Functions.py
+++ Functions.py
@@ -4,3 +4,11 @@ Functions in Python
 Functions are one of the most important topics in Python.
 They will later become extremely important for FastAPI, AI/ML, data science, and larger projects.
 """
+
+
+# Problem 1 — Simple Function
+
+def hello():
+    print("Hello World!")
+
+hello()
