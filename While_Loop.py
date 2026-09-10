fc97aa5eb63c8a438ac4092d0bdeef6ffd405d26
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-10 10:03:08 +0500

    Update

--- While_Loop.py
+++ While_Loop.py
@@ -9,3 +9,27 @@
 # while i <= 10:
 #     print(i)
 #     i += 1
+
+
+
+# # Problem 2 — Sum of Numbers
+# # Statement
+
+# # Ask the user for a number n.
+
+# # Use a while loop to calculate the sum from 1 to n.
+
+
+
+# num = float(input("Enter a number: "))
+
+# sum = 0
+
+# i = 1
+# while i <= num:
+#     sum += i
+#     i += 1
+
+# print(f"The sum of numbers from 1 to {num} is {sum}")
+
+
