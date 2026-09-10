0b9c9eed6f0211b44ccd912a463342c834a805fe
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-10 10:10:29 +0500

    Update

--- While_Loop.py
+++ While_Loop.py
@@ -32,4 +32,22 @@
 
 # print(f"The sum of numbers from 1 to {num} is {sum}")
 
+# # Problem 3 — Even Numbers
+# # Statement
 
+# # Ask the user for a number n.
+
+# # Use a while loop to print all even numbers from 1 to n.
+
+
+# # 1. Take input from the user
+# n = int(input("Enter a number (n): "))
+
+# # 2. Initialize the loop counter
+# i = 1
+
+# # 3. Loop until i goes past n
+# while i <= n:
+#     if i % 2 == 0:  # Checks if i is divisible by 2 with 0 remainder
+#         print(i)
+#     i += 1  # Increment counter (CRITICAL: prevents infinite loop)
