2449ad7ca94d45ab66c3d1aec5e618f11a58c73a
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-10 10:46:48 +0500

    update

---
+++ Functions.py
@@ -0,0 +1,6 @@
+"""
+Functions in Python
+
+Functions are one of the most important topics in Python.
+They will later become extremely important for FastAPI, AI/ML, data science, and larger projects.
+"""
