678b270dfc9425f8de23d4ef72dc338bdd471af0
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-14 20:13:40 +0500

    Updating

--- Variable_solving.py
+++ Variable_solving.py
@@ -1,18 +1,22 @@
-a =10 
-b = 20
-c= a+ b
-print(c)
-
-print("Hello World")
+# a =10 
 
 
-a= 'Ali'
-b= 'Ahmad'
+# b = 20
+# c= a+ b
+# print(c)
 
-print(a)
-print(b)
-print(a+b)
+# print("Hello World")
 
-print(b + a)
+
+# a= 'Ali'
+# b= 'Ahmad'
+# c= a+b
+# d= b+a
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(d)
 
 
