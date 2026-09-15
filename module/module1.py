4ab03498d5a68feaf11b98b49b56a7acead10444
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-09-15 10:39:34 +0500

    update

---
+++ module/module1.py
@@ -0,0 +1,18 @@
+# Problem No 1 
+
+# import math 
+# print(math.sqrt(49))
+
+
+# # Problem No 2
+
+# from math import sqrt
+# print(sqrt(16))
+
+
+
+
+# #Problem no 3
+
+# import mymodule
+# print(mymodule.greet())
